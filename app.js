241e8ad0cec4b23f6b13d59bb3a08e7c716fa7dc
gary909 <garywhite61@hotmail.com>
2023-05-22 12:45:22 +0200

    code

---
+++ app.js
@@ -0,0 +1,8 @@
+function largestPairSum (numbers) {
+  //TODO: Write your Code here
+}
+
+console.log(numbers([10,14,2,23,19])); // 42
+console.log(numbers([-100,-29,-24,-19,19])); // 0
+console.log(numbers([1,2,3,4,6,-1,2])); // 10
+console.log(numbers([-10, -8, -16, -18, -19])); // -18
