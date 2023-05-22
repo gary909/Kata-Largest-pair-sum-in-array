bb57f1d0ddeb455813a02130484b21b1c692299a
gary909 <garywhite61@hotmail.com>
2023-05-22 12:50:24 +0200

    code working

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 function largestPairSum (numbers) {
   //TODO: Write your Code here
+  return numbers.sort((a,b)=>b-a)[0] +numbers.sort((a,b)=>b-a)[1];
 }
 
-console.log(numbers([10,14,2,23,19])); // 42
-console.log(numbers([-100,-29,-24,-19,19])); // 0
-console.log(numbers([1,2,3,4,6,-1,2])); // 10
-console.log(numbers([-10, -8, -16, -18, -19])); // -18
+console.log(largestPairSum([10,14,2,23,19])); // 42
+console.log(largestPairSum([-100,-29,-24,-19,19])); // 0
+console.log(largestPairSum([1,2,3,4,6,-1,2])); // 10
+console.log(largestPairSum([-10, -8, -16, -18, -19])); // -18
